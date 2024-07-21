7c58db9ae0937ccbf97efd71ae7dd2f005d6aae6
Anantha Krishnan <jananthak@gmail.com>
2024-07-21 23:56:31 +0530

    nav

--- app/Components/Nav.tsx
+++ app/Components/Nav.tsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav className='w-screen h-16 items-center flex'>
+      <ul className='w-full flex justify-evenly flex-row'>
+        <li className='hover:cursor-pointer'>Home</li>
+        <li className='hover:cursor-pointer'>Recent</li>
+        <li className='hover:cursor-pointer'>Hot deals</li>
+        <li className='hover:cursor-pointer'>Latest</li>
+        <li className='hover:cursor-pointer'>About</li>
+      </ul>
+    </nav>
   )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Nav from "./Components/Nav";
 
 export default function Home() {
   return (
   <>
-  
+  <Nav/>
   </>
   );
 }
