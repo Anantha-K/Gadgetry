10a646071898e14a9e3c8e7f7f4e30fdbaf54067
Anantha Krishnan <jananthak@gmail.com>
2024-09-18 18:01:47 +0530

    first

---
+++ app/[slug]/page.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+    <div className='bg-gray-900 w-full h-screen flex align-middle justify-center'>
+        <div className='bg-white w-[90%] h-screen'>
+            <h1>Heading</h1>
+            
+
+        </div>
+
+    </div>
+    </>
+  )
+}
+
+export default page
